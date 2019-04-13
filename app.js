933a677a4fc2ede467d88e9609c3f828295d13e8
Eric Lingren <ericlingren@gmail.com>
2019-04-12 23:14:28 -0600

    adding initial commit, starting to add coding challenges

---
+++ app.js
@@ -0,0 +1,16 @@
+//Create a function that takes two numbers as arguments and return their sum.
+
+// Examples
+// addition(3, 2) ➞ 5
+// addition(-3, -6) ➞ -9
+// addition(7, 3) ➞ 10
+
+function addition(a, b) {
+    let sum = a + b
+    console.log(sum)
+}
+
+// addition(7,3)
+
+
+
