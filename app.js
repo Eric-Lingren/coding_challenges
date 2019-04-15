fea566abc3afd19b371ee7e9a015c5bf22629040
Eric Lingren <ericlingren@gmail.com>
2019-04-15 08:51:05 -0600

    updating coding challenges

--- app.js
+++ app.js
@@ -14,3 +14,15 @@ function addition(a, b) {
 
 
 
+///////////////////////////////////////////////////////////////
+//      Return the Next Number from the Integer Passed       //
+///////////////////////////////////////////////////////////////
+
+// Create a function that takes a number as an argument, increments the number by +1 and returns the result.
+
+// Examples
+// addition(0) ➞ 1
+
+// addition(9) ➞ 10
+
+
