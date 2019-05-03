25c73fa6eda26caef0da3ee99f1e2d1b59b46d51
Eric Lingren <ericlingren@gmail.com>
2019-05-03 09:57:54 -0600

    added a new coding challenge

--- README.md
+++ README.md
@@ -3,4 +3,4 @@
 
 This repo is an in progress project of simple and complex coding callenges.  
 
-To see the results, uncomment the function calls and run a npm start.
+To see the results, uncomment the function calls and run a node app.js
--- app.js
+++ app.js
@@ -26,3 +26,30 @@ function addition(a, b) {
 // addition(9) ➞ 10
 
 
+
+
+///////////////////////////////////////////////////////////////
+//    Write a function which takes in an array of numbers    //
+//    and puts all the same values in subarrays like for     //
+//     example [1,2,1,2] should return [[1,1], [2,2]]        //
+///////////////////////////////////////////////////////////////
+
+const inputArr = [1,2,1,2]
+
+function subArrays(inputArr){
+    let checkedNumbers = []
+    let finalArr = []
+
+    for(let i = 0; i < inputArr.length; i++){
+        let hasBeenChecked = checkedNumbers.some(number => number === inputArr[i])
+        if(!hasBeenChecked){
+        checkedNumbers.push(inputArr[i])
+        let filtered = inputArr.filter(number => number === inputArr[i])
+        finalArr.push(filtered)
+        }
+    }
+    console.log(finalArr)
+}
+
+// subArrays(inputArr)
+
