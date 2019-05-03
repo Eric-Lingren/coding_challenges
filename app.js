cf215bee9a6277b24b3036a44196ab29bc72f1e5
Eric Lingren <ericlingren@gmail.com>
2019-05-03 10:14:23 -0600

    finished addition challenge

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 //Create a function that takes two numbers as arguments and return their sum.
 
-// Examples
+// Examples:
 // addition(3, 2) ➞ 5
 // addition(-3, -6) ➞ -9
 // addition(7, 3) ➞ 10
@@ -14,24 +14,32 @@ function addition(a, b) {
 
 
 
+
+
 ///////////////////////////////////////////////////////////////
 //      Return the Next Number from the Integer Passed       //
 ///////////////////////////////////////////////////////////////
 
 // Create a function that takes a number as an argument, increments the number by +1 and returns the result.
 
-// Examples
+// Examples:
 // addition(0) ➞ 1
-
 // addition(9) ➞ 10
+
+function addition(num){
+    num++
+    console.log(num)
+}
+
+// addition(0)
 
 
 
 
 ///////////////////////////////////////////////////////////////
 //    Write a function which takes in an array of numbers    //
-//    and puts all the same values in subarrays like for     //
-//     example [1,2,1,2] should return [[1,1], [2,2]]        //
+//      and puts all the same values in subarrays. For       //
+//      example [1,2,1,2] should return [[1,1], [2,2]]       //
 ///////////////////////////////////////////////////////////////
 
 const inputArr = [1,2,1,2]
@@ -43,9 +51,9 @@ function subArrays(inputArr){
     for(let i = 0; i < inputArr.length; i++){
         let hasBeenChecked = checkedNumbers.some(number => number === inputArr[i])
         if(!hasBeenChecked){
-        checkedNumbers.push(inputArr[i])
-        let filtered = inputArr.filter(number => number === inputArr[i])
-        finalArr.push(filtered)
+            checkedNumbers.push(inputArr[i])
+            let filtered = inputArr.filter(number => number === inputArr[i])
+            finalArr.push(filtered)
         }
     }
     console.log(finalArr)
