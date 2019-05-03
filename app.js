209ceb1b382a52d1fa6feea95c615fffadc50e5b
Eric Lingren <ericlingren@gmail.com>
2019-05-03 10:38:58 -0600

    adding more challenges to projects

--- app.js
+++ app.js
@@ -61,3 +61,94 @@ function subArrays(inputArr){
 
 // subArrays(inputArr)
 
+
+
+
+
+
+///////////////////////////////////////////////////////////////
+//                                                           //
+//                     3 Longest Words                       //
+//                                                           //
+///////////////////////////////////////////////////////////////
+
+//  Write a function that takes an array of words and returns the 3 longest adjacent words as the result
+
+//  Example:
+//  [ "this", "is", "an", "array"] should return ["is", "an", "array"]
+
+function combined(arr){
+    let max = 0;
+    let result = []
+    for(let i = 0; i < arr.length - 2; i++){
+        let currentLength = arr[i].length + arr[i + 1].length + arr[i + 2].length
+        let currentWords = [arr[i], arr[i + 1], arr[i + 2]]
+        if(currentLength > max){
+            result = currentWords
+            max = currentLength
+        }
+    }
+    console.log(result)
+}
+
+// combined(["this", "is", "an", "array"])
+
+
+
+
+
+
+///////////////////////////////////////////////////////////////
+//                                                           //
+//                        Total Area                         //
+//                                                           //
+///////////////////////////////////////////////////////////////
+
+//  Write a constructor function that creates a square or rectangle which can calculate its area
+
+//  Example:
+//  (10,10) should return 100
+//  (7, 12) should return 84
+
+class Rectangle {
+    constructor(height, width) {
+        this.height = height;
+        this.width = width;
+    }
+    // Getter
+    get area() {
+        return this.calcArea();
+    }
+    // Method
+    calcArea() {
+      return this.height * this.width;
+    }
+}
+
+// const square = new Rectangle(7, 12);
+// console.log(square.area);
+
+
+
+
+
+
+///////////////////////////////////////////////////////////////
+//                                                           //
+//                        X's & O's                          //
+//                                                           //
+///////////////////////////////////////////////////////////////
+
+//  Write a function that determines if a string has the same number of X's and O's
+
+//  Example:
+//  "ooxxxxx" should return false
+//  "oXxOox" should return true
+
+function XO(str) {
+    let x = str.toLowerCase().split('').filter(x => x === 'x').length;
+    let o = str.toLowerCase().split('').filter(x => x === 'o').length;
+    console.log( x === o );
+}
+
+// XO("oXxOox")
