d3092bf73c68d7d1d7207e0b0c517a42422f9b3f
Eric-Lingren <44101756+Eric-Lingren@users.noreply.github.com>
2019-08-22 22:02:27 -0600

    adding better instructions for mobile experience

--- src/ChallengeAggregator.js
+++ src/ChallengeAggregator.js
@@ -9,6 +9,7 @@ import Challenge06 from './challenges/challenge06'
 function ChallengeAggregator() {
     return (
         <div>
+            <h3> On mobile, you must scroll left within the black boxes to see the code.  This is part of the Prismjs package I am using to display inline code. </h3>
             <Challenge01 />
             <Challenge02 />
             <Challenge03 />
