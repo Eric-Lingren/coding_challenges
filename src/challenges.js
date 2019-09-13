3042df230f2b637cb924c798d55ef344e4cf5624
Eric Lingren <ericlingren@gmail.com>
2019-09-13 09:57:50 -0600

    adding challenge

--- src/challenges.js
+++ src/challenges.js
@@ -303,4 +303,39 @@ function pancakeFlipper(pancakes){
     console.log(`Flip Count was ${flipCount}`)
 }
 
-// pancakeFlipper(test1)
+//  pancakeFlipper(test1)
+
+
+
+
+
+
+
+
+
+// Given a string, replace every letter with its position in the alphabet.
+
+// If anything in the text isn't a letter, ignore it and don't return it.
+
+// "a" = 1, "b" = 2, etc.
+
+// Example
+// alphabetPosition("The sunset sets at twelve o' clock.")
+// Should return "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11" (as a string)
+
+alphabetPosition = (string) => {
+    const alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    let resultArray = []
+
+    for(let i = 0; i < string.length; i++ ){
+        let currentLetter = string.charAt(i).toLowerCase()
+        if(alphabet.indexOf(currentLetter) > -1){
+            resultArray.push(alphabet.indexOf(currentLetter))
+        }
+    }
+
+    const resultString = resultArray.join(' ')
+    return resultString
+}
+
+alphabetPosition("The sunset sets at twelve o' clock.")
